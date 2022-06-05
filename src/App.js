c5988786cf290c5f329d550023039c61c6fc848a
ikikika <iki.jito@gmail.com>
2022-06-06 03:18:07 +0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Clock from "react-live-clock";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>
+        <Clock format="HH:mm:ss" interval={1000} ticking={true} />
+      </h1>
     </div>
   );
 }
